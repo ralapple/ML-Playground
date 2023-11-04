400509e0104e62d58939293682d359cbe5395d40
ralapple <ralapple@thelyonfamily.net>
2023-11-03 22:00:14 -0500

    initializing KMeans
    working on implementing a KMeans algorithm that was learned in class.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ml-training
 Playing around with ML
+
+
+### Methods That I have Learned in Class
+- K means
+- 
---
+++ k-means.py
@@ -0,0 +1,35 @@
+'''
+K means classification
+
+'''
+import numpy as np
+import data
+
+
+
+
+class KMeans:
+    def __init__(self) -> None:
+        self.clusters = 1
+        self.means = np.array[self.clusters]
+
+    def assing(self,data):
+        '''
+        Assigns the datapoints to the new centers
+        @param: data: the data points
+        @updates: 
+        '''
+        pass
+
+    def update_centers(self):
+        '''
+        Updates the cluter centers
+        @Param: 
+        '''
+        pass
+
+
+
+
+if __name__ == "__main__":
+    print("Hello world")
