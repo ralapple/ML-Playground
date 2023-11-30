2552e7b714639e74f2cc96c8c8c6d78be3220642
ralapple <ralapple@thelyonfamily.net>
2023-11-30 11:44:56 -0600

    Starting Pytorch
    Hoping to learn how to do simple image detection.

---
+++ image_detection.py
@@ -0,0 +1,25 @@
+'''
+Created:        November 30th, 2023
+Last Updated:   November 30th, 2023
+Testing a simple image detection model, first time using pytorch
+'''
+
+import torch
+
+
+
+class AirplaneDetector:
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
