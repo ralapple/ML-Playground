0cb4c70ebb035f40da191ecdf67318547fd5e5ec
ralapple <ralapple@thelyonfamily.net>
2023-12-07 22:40:53 -0600

    Comments for learning
    Trying to fix the errors with image dimensions.

--- detector/image_detection.py
+++ detector/image_detection.py
@@ -1,39 +1,62 @@
 '''
 Created:        November 30th, 2023
-Last Updated:   November 30th, 2023
+Last Updated:   December 7th, 2023
 Testing a simple image detection model, first time using pytorch
 '''
-import os
-from PIL import Image, ImageDraw
+from PIL import Image
 import numpy as np
 import torch
 import torchvision.transforms as T
-from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
 from torchvision.utils import draw_bounding_boxes
 
 
-
-
+"""
+Image detection algorithm using pytorch
+"""
 class AirplaneDetector:
     def __init__(self, model_path=None, num_classes=91):
+
+        # if a model is provided, use that model
         if model_path:
             self.model = torch.load(model_path)
         else:
+            # otherwise use the pretrained model from pytorch vision
             self.model = fasterrcnn_resnet50_fpn(pretrained=True)
+
+            # ROIHeads is the class for region of interest heads - handle the detection tasks
+            # box_predictor is the class for the box predictor - predicts the bounding box coordinates around the object and class scores
+            # cls_score is the classification layer of the box predictor - predicts the class scores for each region proposal
+            # in_features is the variable that stores the number of input features to the classification layer
+                # It's important because when you modify the model (in the line below), you need to know the number of input features for the new layer you are replacing it with.
             in_features = self.model.roi_heads.box_predictor.cls_score.in_features
+
+            # 
             self.model.roi_heads.box_predictor.cls_score = torch.nn.Linear(in_features, num_classes + 1)
         
         self.model.eval()
         self.transforms = T.Compose([T.ToTensor()])
 
-
     def train(self):
+        '''
+        Training function for the model when creating our own CNN
+        '''
         pass
 
     def predict(self, image_path):
+        '''
+        Predicts the bounding boxes for the image
+        '''
+
+        # Preprocess the image using function below
         image_array = preprocess_image(image_path)
+
+        # Convert the image to a tensor
         image_tensor = self.transforms(image_array)
+
+        # Change the tensor values to floats - for the pytorch model
         image_tensor = image_tensor.float()
+
         image_tensor = image_tensor.unsqueeze(0)  # Add batch dimension
 
         # Pass the image to the model
@@ -52,13 +75,14 @@ class AirplaneDetector:
         Image.fromarray(image_with_boxes.mul(255).permute(1, 2, 0).byte().numpy()).show()
 
     def evaluate(self):
-
         pass
 
 
 
 
 def preprocess_image(image_path):
+
+    # Normalize the image values
     new_min = 0
     new_max = 255
 
@@ -75,7 +99,6 @@ def preprocess_image(image_path):
     image_arr = (image_arr - old_min) * ((new_max - new_min) / (old_max - old_min)) + new_min
 
     return image_arr
-
 
 def convert_image(image_arr):
     '''
