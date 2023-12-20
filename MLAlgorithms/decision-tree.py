138f99db9ef5ff9142ed20ca71da51180316c2a0
ralapple <ralapple@thelyonfamily.net>
2023-12-19 22:45:11 -0600

    Changes

---
+++ MLAlgorithms/decision-tree.py
@@ -0,0 +1,16 @@
+'''
+Decision Tree Algorithm from scratch
+Coming Soon...
+'''
+
+
+class DecisionTree:
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, X, y):
+
+
+
+def calculate_entropy():
+    pass
