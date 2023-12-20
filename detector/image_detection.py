d4d0b5fc9940cb1f866a0f2c4472644e0b2af8d7
ralapple <ralapple@thelyonfamily.net>
2023-12-19 21:06:23 -0600

    Working image classification program
    Utilizes Pytorch torchvision. Working vision, will output the class of the detected object.

--- detector/image_detection.py
+++ detector/image_detection.py
@@ -6,9 +6,10 @@ Testing a simple image detection model, first time using pytorch
 from PIL import Image
 import numpy as np
 import torch
-import torchvision.transforms as T
-from torchvision.models.detection import fasterrcnn_resnet50_fpn
-from torchvision.utils import draw_bounding_boxes
+
+from torchvision.transforms import v2 as T
+from torchvision.io import read_image
+from torchvision.models import resnet50, ResNet50_Weights
 
 
 
@@ -18,27 +19,25 @@ class AirplaneDetector:
     Identify airplanes in an image
     Starting using a pretrained model from pytoch vision
     '''
-    def __init__(self, model_path=None, num_classes=91):
+    def __init__(self, model_path=None):
 
         # if a model is provided, use that model
         if model_path:
             self.model = torch.load(model_path)
+
         else:
             # otherwise use the pretrained model from pytorch vision
-            self.model = fasterrcnn_resnet50_fpn(pretrained=True)
+            self.weights = ResNet50_Weights.IMAGENET1K_V2
+            self.model = resnet50(weights=self.weights)
 
             # ROIHeads is the class for region of interest heads - handle the detection tasks
             # box_predictor is the class for the box predictor - predicts the bounding box coordinates around the object and class scores
             # cls_score is the classification layer of the box predictor - predicts the class scores for each region proposal
             # in_features is the variable that stores the number of input features to the classification layer
                 # It's important because when you modify the model (in the line below), you need to know the number of input features for the new layer you are replacing it with.
-            in_features = self.model.roi_heads.box_predictor.cls_score.in_features
-
-            # 
-            self.model.roi_heads.box_predictor.cls_score = torch.nn.Linear(in_features, num_classes + 1)
-        
-        self.model.eval()
-        self.transforms = T.Compose([T.ToTensor()])
+            
+            # sets the model to eval mode
+            self.model.eval()
 
     def train(self) -> None:
         '''
@@ -46,69 +45,45 @@ class AirplaneDetector:
         '''
         pass
 
-    def predict(self, image_path) -> None:
+    def predict(self, image_path, num_predictions=5) -> None:
         '''
         Predicts the bounding boxes for the image and casts them on the image
         '''
+        batch = self.preprocess(image_path)
 
-        # Preprocess the image using function below
-        image_array = preprocess_image(image_path)
+        prediction = self.model(batch).squeeze().softmax(0)
 
-        # Convert the image to a tensor
-        image_tensor = self.transforms(image_array)
+        topk_probability, topk_indices = torch.topk(prediction, num_predictions)
 
-        # Change the tensor values to floats - for the pytorch model
-        image_tensor = image_tensor.float()
+        for i in range(num_predictions):
+            label = topk_indices[i].item()
+            score = topk_probability[i].item()
+            category = self.weights.meta["categories"][label]
+            print(f'Class: {category} | Probability: {score * 100:.2f}') 
 
-        image_tensor = image_tensor.unsqueeze(0)  # Add batch dimension
 
-        # Pass the image to the model
-        with torch.no_grad():
-            predictions = self.model(image_tensor)
+    def preprocess(self, image_path):
+        '''
+        Preprocesses the image
+        '''
 
-        # Print model predictions
-        print(predictions)
+        # Open the image
+        image = read_image(image_path)
 
-        # Extract bounding boxes and labels
-        boxes = predictions[0]['boxes'].cpu().numpy()
-        labels = predictions[0]['labels'].cpu().numpy()
+        preprocess = self.weights.transforms(antialias=True)
 
-        # Visualize bounding boxes on the image
-        image_with_boxes = draw_bounding_boxes(image_array, boxes, labels)
-        Image.fromarray(image_with_boxes.mul(255).permute(1, 2, 0).byte().numpy()).show()
+        # preprocess the image
+        batch = preprocess(image).unsqueeze(0)
 
-def preprocess_image(image_path):
+        return batch
 
-    # Normalize the image values
-    new_min = 0
-    new_max = 255
-
-    # Open image and create an array of the pixel values
-    image = Image.open(image_path)
-    
-    # Convert the image to RGB (remove alpha channel if present)
-    image = image.convert("RGB")
-    
-    image_arr = np.array(image)
-
-    # Normalize the values of the image
-    old_min, old_max = image_arr.min(), image_arr.max()
-    image_arr = (image_arr - old_min) * ((new_max - new_min) / (old_max - old_min)) + new_min
-
-    return image_arr
-
-def convert_image(image_arr):
-    '''
-    converts the image to useable format for the model
-    param: image_arr: array of the image
-    '''
-
-    # Convert the image to a tensor
-    pass
 
 
 if __name__ == "__main__":
     import ssl
     ssl._create_default_https_context = ssl._create_unverified_context
 
-    AirplaneDetector(model_path=None).predict('./airplane.png')
+    # AirplaneDetector(model_path=None).predict('./airplane.png')
+
+    a1 = AirplaneDetector()
+    a1.predict('./a.jpeg')
