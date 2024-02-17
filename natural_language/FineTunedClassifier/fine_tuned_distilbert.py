7de254469c0bf58086f1c9833ec40a7a8602ce53
ralapple <ralapple@thelyonfamily.net>
2024-02-16 19:19:12 -0600

    Formatted the fine tuned model

--- natural_language/FineTunedClassifier/fine_tuned_distilbert.py
+++ natural_language/FineTunedClassifier/fine_tuned_distilbert.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Fine tuning a model using dataset from Hugging Face
+
 """
 import torch
 import math
@@ -13,76 +14,81 @@ import numpy as np
 from datasets import load_dataset
 from transformers import AutoTokenizer, DataCollatorWithPadding, AutoModelForSequenceClassification, TrainingArguments, Trainer, pipeline
 
+custom_train = True
+
 if torch.cuda.is_available():
   device = 'cuda:0'
 else:
   device = 'cpu'
-
 print(f'device: {device}')
 
-train_ds = load_dataset('yaful/DeepfakeTextDetect', split="validation")
+train_ds = load_dataset('yaful/DeepfakeTextDetect', split="train")
 test_ds = load_dataset('yaful/DeepfakeTextDetect', split="test")
 
 print(f"Length of train_ds: {len(train_ds)}")
 print(f"Length of test_ds: {len(test_ds)}")
 
-
+# Set the shuffle seed
 my_seed = 19
 
+# Shuffle data
 train_ds = train_ds.shuffle(seed=my_seed)
 test_ds = test_ds.shuffle(seed=my_seed)
 
-"""#### Check the Number of Each Label to Ensure Even Split"""
-
+# Lengths of the data
 print(f"Number of training samples computer written: {len(train_ds.filter(lambda count: count['label'] == 0))}")
 print(f"Number of trainng samples human written: {len(train_ds.filter(lambda count: count['label'] == 1))}\n")
 
+# Check split between labels
 print(f"Number of test samples computer written: {len(test_ds.filter(lambda count: count['label'] == 0))}")
 print(f"Number of test samples human written: {len(test_ds.filter(lambda count: count['label'] == 1))}")
 
+# Hugging face model
 my_model_name = 'distilbert-base-uncased'
 
+# Text tokenizer
 my_tokenizer = AutoTokenizer.from_pretrained(my_model_name)
 
-
+# Preprocess text
 def preprocess_function(examples):
-  return my_tokenizer(examples['text'], truncation=True)
+    """
+    Tokenizes the data
+    """
+    return my_tokenizer(examples['text'], truncation=True)
 
-print("tokenizing train_ds")
+
+# Tokenize the datasets
 tokenized_train_ds = train_ds.map(preprocess_function, batched=True)
-
-print("tokenizing test_ds")
 tokenized_test_ds = test_ds.map(preprocess_function, batched=True)
 
+# Data collator
 data_collator = DataCollatorWithPadding(tokenizer=my_tokenizer)
-
-"""### Evaluation Used in Training"""
 
 accuracy = evaluate.load('accuracy')
 
 def compute_metrics(eval_pred):
-  predictions, labels = eval_pred
-  predictions = np.argmax(predictions, axis=1)
-  return accuracy.compute(predictions=predictions, references=labels)
-
-"""## Model Training
-
-#### Initialize Labels and Model
-"""
+    """
+    Computes the metrics while training
+    """
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
 
 
-
+# Initialize the labels for the data
+# Makes them human readable
 labels = ['COMPUTER', 'HUMAN']
 id2label = {i: label for i, label in enumerate(labels)}
 label2id = {label: i for i, label in id2label.items()}
 
+# Ensures labels are set
 print('id2label:', id2label)
 print('label2id:', label2id)
 
+# Load the model from hugging face
 model = AutoModelForSequenceClassification.from_pretrained(my_model_name, num_labels=len(labels), id2label=id2label, label2id=label2id)
 
-"""#### Set up Training Args"""
-
+# Training Parameters
 epochs = 8
 lr = 2e-6
 
@@ -98,12 +104,8 @@ train_args = TrainingArguments(
     load_best_model_at_end=True,
 )
 
-"""#### Weights and Biases for Tracking"""
-
-
+# Init weights and biases for training tracking
 wandb.login()
-
-
 wandb.init(
     # set the wandb project where this run will be logged
     project="ray_rays_project",
@@ -117,8 +119,8 @@ wandb.init(
     }
 )
 
-"""## PREBUILT TRAINER CLASS"""
 
+# Default Training class given by hugging face
 trainer = Trainer(
     model=model,
     args=train_args,
@@ -129,17 +131,12 @@ trainer = Trainer(
     compute_metrics=compute_metrics,
 )
 
-trainer.train()
-
-"""*italicized text*
-
-## New MyTrainer Class
-
-Create myTrainer class and override the inner training loop, the rest will be the same
-"""
+if custom_train is False:
+    trainer.train()
 
 
 
+# Custom trainer class to finetune the training parameters
 class MyTrainer(Trainer):
   def _inner_training_loop(self, batch_size=None, args=None, resume_from_checkpoint=None, trial=None, ignore_keys_for_eval=None):
     number_of_epochs = args.num_train_epochs
@@ -194,7 +191,6 @@ class MyTrainer(Trainer):
           # similar to the training loop that does this
 
           eval_epoch.set_description(f"Evaluation Epoch {epoch + 1}")
-          # ... TODO implement by yourslef
 
           for eval_step, eval_input in enumerate(eval_epoch):
             eval_inputs = eval_input.to(device)
@@ -237,22 +233,17 @@ my_train = MyTrainer(
 my_train.train()
 wandb.finish()
 
-"""## Save the Model for Later Use"""
-
+# Save the model locally
 my_train.save_model("./my_model")
 
+# Push the model to hugging face for saving
 model.push_to_hub('lyon0210/fine-tuned-model-ray')
 
-"""## If Model has Been Saved, Load it Here"""
-
-
+# Load the model once saved
 path = './my_model'
 model = AutoModelForSequenceClassification.from_pretrained(path)
 
-"""## Apply the Model to New Input"""
-
-
-
+# Applying the model
 text_to_classify = ""
 
 classifier = pipeline('sentiment-analysis', model = model, tokenizer=my_tokenizer, device=device)
@@ -265,11 +256,3 @@ with torch.no_grad():
 predicted_class_id = logits.argmax().item()
 print(logits.argmax().item())
 model.config.id2label[predicted_class_id]
-
-"""## Visualize the Data"""
-
-
-
-
-data_to_viz = []
-plt = None
